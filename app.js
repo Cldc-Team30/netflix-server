e14ad560eb5413aa053765c0fb3e7335e36d7eee
Nandika Jain <jainnandika@gmail.com>
2022-12-05 21:04:13 +0530

    Update app.js

--- app.js
+++ app.js
@@ -40,14 +40,32 @@ const netflixSchema = new mongoose.Schema({
 });
 
 const userSchema = new mongoose.Schema({
-    userId: {},
-    minsEduStreamed: {},
-    minsDramaStreamed: {},
-    payment: {}
+    userId: {
+        type: Number,
+        required: true,
+        unique: true,
+    },
+    userEmail: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    minsEduStreamed: {
+        type: Number,
+        required: true,
+        min: 0,
+    },
+    minsDramaStreamed: {
+        type: Number,
+        required: true,
+        min: 0,
+    }
 })
 
 const Video = mongoose.model("Video", netflixSchema);
 const User = mongoose.model("User", userSchema);
+
+// User.create({userId:1, userEmail: "test@gmail.com", minsEduStreamed:12, minsDramaStreamed:10})
 
 // Video.insertMany([
 //     {
@@ -80,58 +98,116 @@ const User = mongoose.model("User", userSchema);
 //     console.log(error)
 // });
 
-// /**
-//  * Endpoint to return the list of all the videos.
-//  */
+/**
+ * Endpoint to return the list of all the videos.
+ */
 app.get("/videos", (req, res) => {
-    Video.find({} , (err, foundVideos)=>{
-        if(err){res.send(err);}
-        else{
-          res.send(foundVideos);
+    Video.find({}, (err, foundVideos) => {
+        if (err) { res.send(err); }
+        else {
+            res.send(foundVideos);
         }
-      })
+    })
 })
 
 /**
  * Endpoint to return information about the video with id:{id}
  */
 app.get("/videos/:id", (req, res) => {
-
     let requestVideoId = req.params.id;
-    Video.findOne({id:requestVideoId}, (err, foundVideo)=>{
-        if(err){
-          res.send(err);
+    Video.findOne({ id: requestVideoId }, (err, foundVideo) => {
+        if (err) {
+            res.send(err);
         }
-        else{
-          if(foundVideo){
-            res.send(foundVideo);
-          }
-          else{
-            res.send("No video was found");
-          }
+        else {
+            if (foundVideo) {
+                res.send(foundVideo);
+            }
+            else {
+                res.send("No video was found");
+            }
         }
-      })
+    })
 })
 
 /**
- * Endpoint to return information about the video with id:{id}
+ * Endpoint to return payment information corresponding to the user.
  */
 app.get("/payment", (req, res) => {
-
+    let userId = req.body.userId;
+    User.findOne({ id: userId }, (err, foundUser) => {
+        if (err) {
+            res.status(403).send(err);
+        }
+        else {
+            if (foundUser) {
+                res.status(200).send(500 + 800 * foundUser.minsDramaStreamed - 200 * foundUser.minsEduStreamed)
+            }
+            else {
+                res.sendStatus(403);
+            }
+        }
+    });
 })
 
+/**
+ * Endpoint to update the mins of educational content streamed.
+ */
 app.patch("/updateMinsEduStreamed", (req, res) => {
 
 })
 
+/**
+ * Endpoint to update the mins of drama content streamed.
+ */
 app.patch("/updateMinsDramaStreamed", (req, res) => {
 
 })
 
-app.post("/addUser", (req, res) => {
-
+/**
+ * Endpoint to verify if the user exists, if not, adding the new user in the database.
+ */
+app.post("/verify", (req, res) => {
+    console.log(req)
+    let userId = req.body.userId;
+    let userEmail = req.body.userEmail;
+    User.findOne({ id: userId }, (err, foundUser) => {
+        if (err) {
+            res.status(403).send(err);
+        }
+        else {
+            if (foundUser) {
+                res.sendStatus(200);
+            }
+            else {
+                User.create({
+                    userId: userId,
+                    userEmail: userEmail,
+                    minsEduStreamed: 0,
+                    minsDramaStreamed: 0
+                })
+                res.sendStatus(200);
+            }
+        }
+    });
 })
 
-app.listen(8000,()=>{
+// /**
+//  * To verify the the token is valid or not.
+//  */
+// function verifyToken(req, res, next) {
+//     const bearerHeader = req.headers['authorization'];
+
+//     if (bearerHeader) {
+//         const bearer = bearerHeader.split(' ');
+//         const bearerToken = bearer[1];
+//         req.token = bearerToken;
+//         next();
+//     } else {
+//         res.sendStatus(403);
+//     }
+// }
+
+app.listen(8000, () => {
     console.log("Server started on port 8000");
-  })
+})
