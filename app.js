03de0cc9fb9ab6197f88ddd2633fbcf71b1484cd
ronald_dacey <jishnu19048@iiitd.ac.in>
2022-12-05 21:01:20 +0530

    added streaming endpoint

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+const path = require('path');
 const mongoose = require('mongoose');
 const express = require('express');
 const url = 'mongodb+srv://admin-nandika:test123@netflix-cluster.j0wiwja.mongodb.net/netflixDB';
@@ -131,6 +133,37 @@ app.patch("/updateMinsDramaStreamed", (req, res) => {
 app.post("/addUser", (req, res) => {
 
 })
+
+app.get('/video/:id', (req, res) => {
+  const path = `assets/goofy.mp4`;
+  const stat = fs.statSync(path);
+  const fileSize = stat.size;
+  const range = req.headers.range;
+  if (range) {
+      const parts = range.replace(/bytes=/, "").split("-");
+      const start = parseInt(parts[0], 10);
+      const end = parts[1]
+          ? parseInt(parts[1], 10)
+          : fileSize-1;
+      const chunksize = (end-start) + 1;
+      const file = fs.createReadStream(path, {start, end});
+      const head = {
+          'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+          'Accept-Ranges': 'bytes',
+          'Content-Length': chunksize,
+          'Content-Type': 'video/mp4',
+      };
+      res.writeHead(206, head);
+      file.pipe(res);
+  } else {
+      const head = {
+          'Content-Length': fileSize,
+          'Content-Type': 'video/mp4',
+      };
+      res.writeHead(200, head);
+      fs.createReadStream(path).pipe(res);
+  }
+});
 
 app.listen(8000,()=>{
     console.log("Server started on port 8000");
